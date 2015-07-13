523ec55ce7c4b3e127d2a99b470cc1bffc84f83b
c4off <a.c4off@gmail.com>
2015-07-13 17:21:36 +0300

    finished with triangle

--- Examples/triangle.js
+++ Examples/triangle.js
@@ -11,12 +11,14 @@ window.onload = function init()
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
 
-    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
+    var vertices = new Float32Array([-0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.7, 0.7, 0.9, 0.9, 0.7, 0.9]);
+
+//    var _2vertices = new Float32Array([0.7, 0.7, 0.9, 0.9, 0.7, 0.9]);
 
     //  Configure WebGL
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.viewport( 0, 0, canvas.width-100, canvas.height );
+    gl.clearColor( 0.2, 0.24, 0.014, 0.688 );
 
     //  Load shaders and initialize attribute buffers
 
@@ -35,11 +37,35 @@ window.onload = function init()
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
     render();
+    setTimeout(function(){
+        /*2 triangle*/
+        //  Load shaders and initialize attribute buffers
+
+        var program2 = initShaders( gl, "vertex-shader2", "fragment-shader" );
+        gl.useProgram( program2 );
+
+        // Load the data into the GPU
+
+        var bufferId2 = gl.createBuffer();
+//        gl.bindBuffer( gl.ARRAY_BUFFER, bufferId2 );
+        gl.bufferData( gl.ARRAY_BUFFER, _2vertices, gl.STATIC_DRAW );
+
+        // Associate out shader variables with our data buffer
+
+        var vPosition2 = gl.getAttribLocation( program2, "vPosition2" );
+        gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vPosition2 );
+
+//        render();
+    }, 500);
+
 };
 
 
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+//    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, 6 );
 }
