a8d240a8ca00a275d76be3836b058d4f48505640
c4off <a.c4off@gmail.com>
2015-07-18 00:35:16 +0300

    triangle twist done

--- Experiments/triangle.js
+++ Experiments/triangle.js
@@ -16,18 +16,35 @@ window.onload = function init()
         return [x1, y1];
     };
 
+    var _divideTriangle = function(a, b, c, times){
+        // push the vertices to result array
+        if(times === 0){
+            resultVertices.push(a, b, c);
+        } else{
+            // get coords of dividing vertices
+            var ab = mix(a, b, 0.5);
+            var bc = mix(b, c, 0.5);
+            var ac = mix(a, c, 0.5);
+            // recursively subdivide triangles
+            _divideTriangle(a, ab, ac, times-1);
+            _divideTriangle(b, ab, bc, times-1);
+            _divideTriangle(c, ac, bc, times-1);
+            _divideTriangle(ab, bc, ac, times-1);
+        }
+    };
+
+    var resultVertices = [];
+
+    _divideTriangle([-0.5, -0.5], [0, 0.5], [0.5, -0.5], 4);
+
     var canvas = document.getElementById( "gl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
 
-//    var vertices = new Float32Array([-0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.7, 0.7, 0.9, 0.9, 0.7, 0.9]);
-    // tessilate the triangle.
-    var vertices = [[-0.5, -0.5], [0, 0.5], [0.5, -0.5]];
-//    var vertices = new Float32Array([-0.5, -0.5, 0, 0.5, 0.5, -0.5]);
     var tmpVertice, rotatedVertices = [];
-    vertices.forEach(function(vertice){
+    resultVertices.forEach(function(vertice){
         tmpVertice = _rotateVertice(vertice[0], vertice[1], 1);
         rotatedVertices.push(tmpVertice[0]);
         rotatedVertices.push(tmpVertice[1]);
@@ -37,10 +54,9 @@ window.onload = function init()
     //  Configure WebGL
 
     gl.viewport( 0, 0, canvas.width-100, canvas.height );
-    gl.clearColor( 0.2, 0.24, 0.014, 0.688 );
+    gl.clearColor( 0.9, 0.9, 0.9, 0.688 );
 
     //  Load shaders and initialize attribute buffers
-
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
@@ -48,8 +64,7 @@ window.onload = function init()
 
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, new Float32Array([-0.5, -0.5, 0, 0.5, 0.5, -0.5]), gl.STATIC_DRAW );
-//    gl.bufferData( gl.ARRAY_BUFFER, rotatedVerticesF32, gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(rotatedVerticesF32), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
 
@@ -59,36 +74,12 @@ window.onload = function init()
 
     gl.clear( gl.COLOR_BUFFER_BIT );
 
-    render();
-
-
-//
-//    setTimeout(function(){
-//        /*2 triangle*/
-//        //  Load shaders and initialize attribute buffers
-//
-//        var program2 = initShaders( gl, "vertex-shader2", "fragment-shader" );
-//        gl.useProgram( program2 );
-//
-//        // Load the data into the GPU
-//
-//        var bufferId2 = gl.createBuffer();
-////        gl.bindBuffer( gl.ARRAY_BUFFER, bufferId2 );
-//        gl.bufferData( gl.ARRAY_BUFFER, _2vertices, gl.STATIC_DRAW );
-//
-//        // Associate out shader variables with our data buffer
-//
-//        var vPosition2 = gl.getAttribLocation( program2, "vPosition2" );
-//        gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0, 0 );
-//        gl.enableVertexAttribArray( vPosition2 );
-//
-////        render();
-//    }, 500);
+    render(flatten(rotatedVerticesF32).length/2);
 
 };
 
 
-function render() {
-//    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+function render(number) {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, number);
 }
